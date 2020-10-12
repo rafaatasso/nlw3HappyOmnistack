35dd072cd3e15601867f6dcadab194a2dd059e04
Rafaela Tasso <atassorafaela@gmail.com>
2020-10-12 17:23:34 -0300

    Create web

---
+++ web/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface TitleProps {
+  text: string;
+}
+
+function Title(props: TitleProps) {
+  return <h1>{props.text}</h1>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Title text="Título 1"/>
+      <Title text="Título 2"/>
+      <Title text="Título 3"/>
+      <Title text="Título 4"/>
+    </div>
+  );
+}
+
+export default App;
