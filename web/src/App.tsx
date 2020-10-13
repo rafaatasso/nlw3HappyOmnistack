9bad19c07d87188625fdd7d63714e9c1f441b669
Rafaela Tasso <atassorafaela@gmail.com>
2020-10-12 22:45:03 -0300

    Create Landing Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 
-interface TitleProps {
-  text: string;
-}
+import './styles/global.css';
 
-function Title(props: TitleProps) {
-  return <h1>{props.text}</h1>
-}
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <Title text="Título 1"/>
-      <Title text="Título 2"/>
-      <Title text="Título 3"/>
-      <Title text="Título 4"/>
-    </div>
+    <Routes />
   );
 }
 
---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function OrphanagesMap(){
+    return (
+        <h1>Hello World</h1>
+    );
+}
+
+export default OrphanagesMap;
